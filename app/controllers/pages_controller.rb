e5050ef9b7868e606a6fabdfd26fab9dcc1cd3c6
John Gould <johngould9487@gmail.com>
2018-10-19 10:56:21 +0100

    home page now responsive to differing login states

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if user_signed_in?
+      if !current_user.students.count.zero?
+        @student = current_user.students.first
+      end
+    end
     @newStudent = Student.new
   end
 end
